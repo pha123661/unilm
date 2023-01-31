c0b0131894bf42ef3cd3ba107eb03a7bfd38a47b
Li, Sheng-Wei <pha123661@gmail.com>
2023-01-31 20:01:44 +0800

    Rename scripts

---
+++ vlmo/scripts/Finetune_F30K.sh
@@ -0,0 +1,1 @@
+python run.py with data_root=./arrow/F30K/ num_gpus=2 num_nodes=1 "task_finetune_irtr_f30k_base_image384" per_gpu_batchsize=8 load_path="~/weights/vlmo_base_patch16_224.pt" log_dir="~/logs"
